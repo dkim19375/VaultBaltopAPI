aa22c449a485f138cd75733d4bc77f5e2e37aed4
dkim19375 <miner8mk@gmail.com>
2021-01-02 21:07:04 -0500

    remove others

--- src/main/java/me/dkim19375/vaultbaltop/Entry.java
+++ src/main/java/me/dkim19375/vaultbaltop/Entry.java
@@ -1,54 +1,48 @@
-/*    */ package me.dkim19375.vaultbaltop;
-/*    */ 
-/*    */ import java.util.Map;
-/*    */ 
-/*    */ public final class Entry<K, V> implements Map.Entry<K, V> {
-/*    */   private K key;
-/*    */   private V value;
-/*    */   
-/*    */   public Entry(K key, V value) {
-/* 10 */     this.key = key;
-/* 11 */     this.value = value;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public K getKey() {
-/* 16 */     return this.key;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public V getValue() {
-/* 21 */     return this.value;
-/*    */   }
-/*    */   
-/*    */   public K setKey(K key) {
-/* 25 */     K old = this.key;
-/* 26 */     this.key = key;
-/* 27 */     return old;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public V setValue(V value) {
-/* 32 */     V old = this.value;
-/* 33 */     this.value = value;
-/* 34 */     return old;
-/*    */   }
-/*    */   
-/*    */   public Entry<K, V> setValues(K key, V value) {
-/* 38 */     Entry<K, V> e = new Entry(this.key, this.value);
-/* 39 */     this.key = key;
-/* 40 */     this.value = value;
-/* 41 */     return e;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public String toString() {
-/* 46 */     return (new StringBuilder()).append(this.key).append("=").append(this.value).toString();
-/*    */   }
-/*    */ }
+package me.dkim19375.vaultbaltop;
 
+import java.util.Map;
 
-/* Location:              C:\Users\Na\Documents\IntelliJ\obfuscate\VaultBaltop-1.0.0.jar!\me\dkim19375\vaultbaltop\Entry.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+public final class Entry<K, V> implements Map.Entry<K, V> {
+    private K key;
+    private V value;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    public K setKey(K key) {
+        K old = this.key;
+        this.key = key;
+        return old;
+    }
+
+    @Override
+    public V setValue(V value) {
+        V old = this.value;
+        this.value = value;
+        return old;
+    }
+
+    public Entry<K, V> setValues(K key, V value) {
+        Entry<K, V> e = new Entry<>(this.key, this.value);
+        this.key = key;
+        this.value = value;
+        return e;
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+}
--- src/main/java/me/dkim19375/vaultbaltop/api/BaltopAPIProvider.java
+++ src/main/java/me/dkim19375/vaultbaltop/api/BaltopAPIProvider.java
@@ -1,27 +1,21 @@
-/*    */ package me.dkim19375.vaultbaltop.api;
-/*    */ 
-/*    */ import me.dkim19375.vaultbaltop.VaultBaltop;
-/*    */ import me.dkim19375.vaultbaltop.api.interf.VaultBaltopAPI;
-/*    */ import net.milkbowl.vault.economy.Economy;
-/*    */ 
-/*    */ public class BaltopAPIProvider {
-/*  8 */   private static VaultBaltopAPI apiInstance = null;
-/*    */   
-/*    */   public static VaultBaltopAPI getAPI() {
-/* 11 */     return apiInstance;
-/*    */   }
-/*    */   
-/*    */   public static Economy getEconomy() {
-/* 15 */     return VaultBaltop.getEconomy();
-/*    */   }
-/*    */   
-/*    */   public static void register(VaultBaltopAPI api) {
-/* 19 */     apiInstance = api;
-/*    */   }
-/*    */ }
+package me.dkim19375.vaultbaltop.api;
 
+import me.dkim19375.vaultbaltop.VaultBaltop;
+import me.dkim19375.vaultbaltop.api.interf.VaultBaltopAPI;
+import net.milkbowl.vault.economy.Economy;
 
-/* Location:              C:\Users\Na\Documents\IntelliJ\obfuscate\VaultBaltop-1.0.0.jar!\me\dkim19375\vaultbaltop\api\BaltopAPIProvider.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+public class BaltopAPIProvider {
+    private static VaultBaltopAPI apiInstance = null;
+
+    public static VaultBaltopAPI getAPI() {
+        return apiInstance;
+    }
+
+    public static Economy getEconomy() {
+        return VaultBaltop.getEconomy();
+    }
+
+    public static void register(VaultBaltopAPI api) {
+        apiInstance = api;
+    }
+}
